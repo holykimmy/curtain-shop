bfd3804383002de1fe9d9f5925d27f9c22458ea8
holykimmy <holykimmy@holykimmys-MacBook-Pro.local>
2024-03-03 01:27:32 +0700

    edit origin

--- server/server.js
+++ server/server.js
@@ -32,7 +32,9 @@ mongoose
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000","https://cms-curtain-shop.vercel.app/", // กำหนดโดเมนที่อนุญาตให้เข้าถึง
+
+    // https://cms-curtain-shop.vercel.app/
+    origin: "http://localhost:3000","https://cms-curtain-shop.vercel.app", // กำหนดโดเมนที่อนุญาตให้เข้าถึง
     methods: ["GET", "POST", "PUT", "DELETE"], // กำหนดเมทอดที่อนุญาต
     allowedHeaders: ["Content-Type", "Authorization", "authtoken"],
     credentials: true, // อนุญาตให้ส่งคุกกี้ (cookies) ไปพร้อมกับคำขอ
