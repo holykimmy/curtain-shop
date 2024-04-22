d512e76a4cb300a280f3fe4888c6e65c1f48599a
holykimmy <holykimmy@holykimmys-MacBook-Pro.local>
2024-04-22 21:54:37 +0700

    link

--- curtain-shop/src/components/admin/AddProductPage.jsx
+++ curtain-shop/src/components/admin/AddProductPage.jsx
@@ -372,7 +372,7 @@ function AddProductPage() {
         </form>
       </div>
       <Link
-        to="/dashboard"
+        to="/products-ad"
         type="button"
         class="fixed bottom-0 flex justify-center ml-2 mb-2 w-1/2 px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-white border rounded-lg gap-x-2 sm:w-auto dark:hover:bg-gray-800 dark:bg-gray-900 hover:bg-gray-100 dark:text-gray-200 dark:border-gray-700"
       >
